c607071f8ff8f5ae468dc295fd1288734a4eeb65
HyeminNoh <shrmffkfk@naver.com>
2020-01-11 07:51:34 +0900

    더맵게 문제

---
+++ Programmers/Lv2/moreHot.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int aScoville : scoville) {
+            heap.offer(aScoville);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+            int result = a + (b * 2);
+            heap.offer(result);
+            answer ++;
+        }
+        return answer;
+    }
+}
