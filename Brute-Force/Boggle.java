1af7867e19cb9684ad047b34c2c9d8c9a1a00d13
HyeminNoh <shrmffkfk@naver.com>
2020-02-11 22:17:06 +0900

    [알고스팟]보글 게임 푸는 중

---
+++ Brute-Force/Boggle.java
@@ -0,0 +1,34 @@
+boolean hasWord(int y, int x, String word){
+
+}
+public Class Boggle{
+    public static void main(String[]args){
+        String answer = "";
+        return answer;
+    }
+}
+
+/* 문제 예시 입력, 출력
+input:
+1
+URLPM
+XPRET
+GIAET
+XTNZY
+XOQRS
+6
+PRETTY
+GIRL
+REPEAT
+KARA
+PANDORA
+GIAZAPX
+
+output:
+PRETTY YES
+GIRL YES
+REPEAT YES
+KARA NO
+PANDORA NO
+GIAZAPX YES
+ */
