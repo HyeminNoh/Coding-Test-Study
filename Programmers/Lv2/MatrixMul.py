a33637e588841b26a7276adf915df3c8aed24909
hyeminnoh <shrmffkfk@naver.com>
2020-06-20 23:42:17 +0900

    [프로그래머스] 행렬의 곱셈

---
+++ Programmers/Lv2/MatrixMul.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    answer = []
+    for idx1 in range(len(arr1)):
+        row = []
+        for idx2 in range(len(arr2[0])):
+            tmp = 0
+            for idx3 in range(len(arr1[0])):
+                tmp += arr1[idx1][idx3] * arr2[idx3][idx2]
+            row.append(tmp)
+        answer.append(row)
+    return answer
